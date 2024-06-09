7c0b2f194abec1e3a812bd5416ae9f2b3905824e
VSVwnl <vishnusai9696@gmail.com>
2024-06-09 12:26:35 +0800

    Audio_and_Skybox

--- Assets/Scripts/Unit/MeteorBasic.cs
+++ Assets/Scripts/Unit/MeteorBasic.cs
@@ -5,37 +5,49 @@ using UnityEngine.UI;
 
 public class MeteorBasic : MonoBehaviour
 {
-    
     [Tooltip("The effect of meteor being hit.")]
     [SerializeField] private GameObject meteorHitVFX;
     [Tooltip("The healthbar of the meteor.")]
     [SerializeField] private Slider healthBar;
 
-    [Tooltip("The current helath of the meteor (Cannot exceed maxHealth).")]
+    [Tooltip("The current health of the meteor (Cannot exceed maxHealth).")]
     [SerializeField] private float health = 100;
-    [Tooltip("The maximum helath of the meteor.")]
+    [Tooltip("The maximum health of the meteor.")]
     [SerializeField] private static float maxHealth = 100;
     [Tooltip("The speed of the meteor.")]
     [SerializeField] private static float speed = 5f;
     [Tooltip("The score of the meteor.")]
     [SerializeField] private static int score = 10;
 
+    [Tooltip("Explosion sound clip.")]
+    [SerializeField] private AudioClip explosionSound;
+
     private Vector3 target;
     private Vector3 rotation;
     private ScoreHandler scoreHandler;
     private HeartHandler heartHandler;
     private GameObject body;
+    private AudioSource audioSource;
 
-    void Start(){
-        rotation= new Vector3(0,0,0);
+    void Start()
+    {
+        rotation = new Vector3(0, 0, 0);
         transform.LookAt(target);
         scoreHandler = GameObject.FindGameObjectWithTag("ScoreHandler").GetComponent<ScoreHandler>();
         heartHandler = GameObject.FindGameObjectWithTag("HeartHandler").GetComponent<HeartHandler>();
         body = GameObject.FindGameObjectWithTag("Body").gameObject;
         MeteorBasic.speed += 0.01f;
+
+        // Get or add an AudioSource component
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
     }
 
-    void Update(){
+    void Update()
+    {
         // Move the meteor towards the target
         Vector3 direction = (target - transform.position).normalized;
         transform.position += direction * speed * Time.deltaTime;
@@ -48,29 +60,31 @@ public class MeteorBasic : MonoBehaviour
         GameObject meteorAsset = transform.GetChild(0).gameObject;
         meteorAsset.transform.Rotate(rotation * Time.deltaTime);
 
-        if(Vector3.Distance(target, transform.position) < 5f){
+        if (Vector3.Distance(target, transform.position) < 5f)
+        {
             Destroy(gameObject);
-            if((int)target.x == (int)body.transform.localPosition.x){
+            if ((int)target.x == (int)body.transform.localPosition.x)
+            {
                 heartHandler.decreaseHearts();
             }
         }
 
-        if(Vector3.Distance(target, transform.position) > 30f){
+        if (Vector3.Distance(target, transform.position) > 30f)
+        {
             target.x = body.transform.localPosition.x;
         }
-
     }
-    
-    void OnCollisionEnter (Collision co)
+
+    void OnCollisionEnter(Collision co)
     {
         // If hit by a bullet, subtract health by the damage of the bullet and update healthbar
-        if(co.gameObject.tag == "bullet")  
+        if (co.gameObject.tag == "bullet")
         {
-            BasicProjectile basicProjectile= co.gameObject.GetComponent<BasicProjectile>();
+            BasicProjectile basicProjectile = co.gameObject.GetComponent<BasicProjectile>();
             health = health - basicProjectile.damage;
-            healthBar.value = health/maxHealth; 
+            healthBar.value = health / maxHealth;
         }
-        if(health <= 0)
+        if (health <= 0)
         {
             destroyMeteor();
             scoreHandler.addScore(score);
@@ -82,10 +96,12 @@ public class MeteorBasic : MonoBehaviour
     /// </summary>
     /// <param name="target">Target where meteor moves towards.</param>
     /// <returns>Returns True if target is set.</returns>
-    public bool setTarget(Vector3 target){
+    public bool setTarget(Vector3 target)
+    {
         bool success = false;
         this.target = target;
-        if (this.target != null){
+        if (this.target != null)
+        {
             success = true;
         }
         return success;
@@ -94,8 +110,9 @@ public class MeteorBasic : MonoBehaviour
     /// <summary>
     /// Set speed of the meteor.
     /// </summary>
-    /// <param name="newSpeed">The ammount of speed set </param>
-    public static void setSpeed(float newSpeed){
+    /// <param name="newSpeed">The amount of speed set </param>
+    public static void setSpeed(float newSpeed)
+    {
         MeteorBasic.speed = newSpeed;
     }
 
@@ -103,16 +120,25 @@ public class MeteorBasic : MonoBehaviour
     /// Return speed of the meteor.
     /// </summary>
     /// <return>Returns the speed.</return>
-    public static float getSpeed(){
+    public static float getSpeed()
+    {
         return MeteorBasic.speed;
     }
 
     /// <summary>
     /// Destroy the meteor.
     /// </summary>
-    public void destroyMeteor(){
+    public void destroyMeteor()
+    {
         var impact = Instantiate(meteorHitVFX, transform.position, Quaternion.identity) as GameObject;
         Destroy(impact, 2);
+
+        // Play explosion sound
+        if (explosionSound != null)
+        {
+            audioSource.PlayOneShot(explosionSound);
+        }
+
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/UnitManager/ShooterBasic.cs
+++ Assets/Scripts/UnitManager/ShooterBasic.cs
@@ -15,34 +15,56 @@ public class ShooterBasic : MonoBehaviour
     [Tooltip("How often can the player shoot.")]
     [SerializeField] private static float firerate = 5f;
 
+    [Tooltip("Audio clip for shooting sound.")]
+    [SerializeField] private AudioClip shootingSound;
+
     private float timeToFire = 0;
     private bool leftHand;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // Get or add an AudioSource component
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
         // Fires a projectile when the ammount of finger up is more than 3 based on fire rate
-        if(handTracker.checkHandRecognized()){
-            if(Time.time >= timeToFire){
-                timeToFire = Time.time + 1/firerate;
-                if(handTracker.checkShoot(true))
+        if (handTracker.checkHandRecognized())
+        {
+            if (Time.time >= timeToFire)
+            {
+                timeToFire = Time.time + 1 / firerate;
+                if (handTracker.checkShoot(true))
                 {
                     InstantiateProjectile(handTracker.firstHandOrigin, handTracker.firstHandDirection);
                 }
-                if(handTracker.checkShoot(false))
+                if (handTracker.checkShoot(false))
                 {
                     InstantiateProjectile(handTracker.secondHandOrigin, handTracker.secondHandDirection);
                 }
             }
         }
-        else{
-            if(mouseLook.checkShoot()){
-                if(Time.time >= timeToFire){
-                timeToFire = Time.time + 1/firerate;
-                    if(leftHand){
+        else
+        {
+            if (mouseLook.checkShoot())
+            {
+                if (Time.time >= timeToFire)
+                {
+                    timeToFire = Time.time + 1 / firerate;
+                    if (leftHand)
+                    {
                         leftHand = false;
                         InstantiateProjectile(LHFirePoint.position, mouseLook.viewDirection);
                     }
-                    else{
+                    else
+                    {
                         leftHand = true;
                         InstantiateProjectile(RHFirePoint.position, mouseLook.viewDirection);
                     }
@@ -58,12 +80,20 @@ public class ShooterBasic : MonoBehaviour
     /// <param name="firepoint">The origin point of the projectile.</param>
     /// <param name="direction">The direction of the projectile.</param>
     /// <returns>Returns the projectile created</returns>
-    GameObject InstantiateProjectile(Vector3 firepoint, Vector3 direction){
+    GameObject InstantiateProjectile(Vector3 firepoint, Vector3 direction)
+    {
         GameObject projectileBasicObj = Instantiate(projectileBasic, firepoint, Quaternion.identity) as GameObject;
 
         projectileBasicObj.GetComponent<BasicProjectile>().setProjectileData(direction);
 
         Destroy(projectileBasicObj, 4);
+
+        // Play shooting sound
+        if (shootingSound != null)
+        {
+            audioSource.PlayOneShot(shootingSound);
+        }
+
         return projectileBasicObj;
     }
 
@@ -71,7 +101,8 @@ public class ShooterBasic : MonoBehaviour
     /// Set firerate of the shooter.
     /// </summary>
     /// <param name="newFirerate">The ammount of speed set </param>
-    public static void setFireRate(float newFirerate){
+    public static void setFireRate(float newFirerate)
+    {
         ShooterBasic.firerate = newFirerate;
     }
 
@@ -79,7 +110,8 @@ public class ShooterBasic : MonoBehaviour
     /// Return firerate of the shooter.
     /// </summary>
     /// <return>Returns the speed.</return>
-    public static float getFireRate(){
+    public static float getFireRate()
+    {
         return ShooterBasic.firerate;
     }
 }
