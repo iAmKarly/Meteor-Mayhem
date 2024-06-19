525fd1f40229d24290f26618b49ed553d2fd1c1d
Setsuna1234 <bryansebastiansendjaja@gmail.com>
2024-06-19 21:53:33 +0700

    SceneManagerScript and New Font

---
+++ Assets/Scripts/ChangeScene/SceneManagerScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
