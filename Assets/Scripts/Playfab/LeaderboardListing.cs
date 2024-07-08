623c4880bfdd53a26ceea45bcb7171e4fb299f23
Setsuna1234 <bryansebastiansendjaja@gmail.com>
2024-07-08 10:29:00 +0800

    Leaderboard

---
+++ Assets/Scripts/Playfab/LeaderboardListing.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class LeaderboardListing : MonoBehaviour
+{
+    public TextMeshProUGUI playerNameText; 
+    public TextMeshProUGUI playerScoreText; 
+}
+
